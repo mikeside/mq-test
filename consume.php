d1cc737d2dca9227875d28ff3b60a7a9e89e34bf
Mike <mikehub@aliyun.com>
2022-07-26 19:50:52 +0800

    mq init

---
+++ consume.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * 消费者
+ * 消费者只需要setName队列名即可达到监听队列，从而达到消费队列中的消息（前提是该队列已经创建并绑定交换机）
+ * 一般都是将交换机和队列、绑定操作初始化完成
+ */
+
+$connConfig = [
+    'host' => '',
+    'port' => '',
+    'vhost' => '',
+    'login' => '',
+    'password' => '',
+];
+
+try {
+
+    // 连接
+    $mqConn = new AMQPConnection($connConfig);
+    $mqConn->connect();
+
+    // 通道
+    $mqChannel = new AMQPChannel($mqConn);
+
+    // 队列消费
+    $mqQueue = new AMQPQueue($mqChannel);
+    $mqQueue->setName('direct.sendMail1');
+
+    // 执行消费
+    $mqQueue->consume(function (AMQPEnvelope $envelope, AMQPQueue $queue){
+
+        sleep(5);
+        echo $envelope->getBody() . ' 消费完成✅' . PHP_EOL;
+        $queue->ack($envelope->getDeliveryTag());
+    });
+
+    $mqConn->disconnect();
+
+}catch (Exception $e){
+    exit($e->getMessage());
+}
---
+++ product.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * 生产者
+ * 生产者只需setName存在的交换机名即可
+ * 生产者只需向交换机生产消息，根据routingKey
+ */
+
+$connConfig = [
+    'host' => '',
+    'port' => '',
+    'vhost' => '',
+    'login' => '',
+    'password' => '',
+];
+
+try {
+
+    // 连接
+    $mqConn = new AMQPConnection($connConfig);
+    $mqConn->connect();
+
+    // 通道
+    $mqChannel = new AMQPChannel($mqConn);
+
+    // 交换机
+    $mqExchange = new AMQPExchange($mqChannel);
+    $mqExchange->setName('direct.sendMail');
+
+    // 生产消息
+    for ($i=1;$i<=10;$i++){
+        $message = '发送邮件给用户：' . 'user' .$i;
+        echo $message . PHP_EOL;
+
+        $mqExchange->publish($message, 'key.sendMail', null, ['delivery_mode' => 2]);
+    }
+
+    $mqConn->disconnect();
+
+}catch (Exception $e){
+    exit($e->getMessage());
+}
